54d3c6cf3a511b707698aca55904864ea6e04dd3
Sandra Szwed <sandra.szwed19@gmail.com>
2022-07-31 15:27:56 +0200

    Added N-th Tribonacci Number

---
+++ leetcode/1137. N-th Tribonacci Number/Solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        
+        int d = -1;
+        for(int i = 3; i < n + 1; i++){
+            d = c + b + a;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return d;
+    }
+};
