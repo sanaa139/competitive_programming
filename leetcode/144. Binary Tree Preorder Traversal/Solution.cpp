7ef1e74493e44d45ded83cb2a9610184f4f29ee2
Sandra Szwed <sandra.szwed19@gmail.com>
2022-08-15 10:38:22 +0200

    Added Binary Tree Preorder Traversal

---
+++ leetcode/144. Binary Tree Preorder Traversal/Solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> nodes;
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return nodes;
+    }
+    
+    void preorder(TreeNode* root){
+        if(root != nullptr){
+            nodes.push_back(root->val);
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+    
+};
